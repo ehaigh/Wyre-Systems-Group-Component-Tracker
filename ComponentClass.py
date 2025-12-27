9c2dd57c0851db24f5359a3ad1ed004b4a2878e4
ehaigh <163937157+ehaigh@users.noreply.github.com>
2025-12-27 01:00:54 +0000

    Created component class

---
+++ ComponentClass.py
@@ -0,0 +1,9 @@
+#need to source status class
+
+class Component:
+    def __init__(self, componentId, componentName, quantity, minQuantity, status):
+        this.componentId = componentId
+        this.componentName = componentName
+        this.quantity = quantity
+        this.minQuantity = minQuantity
+        this.status = status
