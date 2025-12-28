aa6145691af33af7bb2396b01ebfb668329f94bb
ehaigh <163937157+ehaigh@users.noreply.github.com>
2025-12-28 00:45:16 +0000

    Update ComponentClass.py

--- ComponentClass.py
+++ ComponentClass.py
@@ -2,8 +2,28 @@
 
 class Component:
     def __init__(self, componentId, componentName, quantity, minQuantity, status):
-        this.componentId = componentId
-        this.componentName = componentName
-        this.quantity = quantity
-        this.minQuantity = minQuantity
-        this.status = status
+        self.componentId = componentId
+        self.componentName = componentName
+        self.quantity = quantity
+        self.minQuantity = minQuantity
+        self.status = status
+    def get_componentId(self):
+        return componentId
+    def get_componentName(self):
+        return componentName
+    def get_quantity(self):
+        return quantity
+    def get_minQuantity(self):
+        return minQuantity
+    def get_status(self):
+        return status
+    def set_componentId(self, componentId):
+        self.componentId = componentId
+    def set_componentName(self, componentName):
+        self.componentName = componentName
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+    def set_minQuantity(self, minQuantity):
+        self.minQuantity = minQuantity
+    def set_status(self, status):
+        self.status = status
