052bd9cd7be500c84d89927ffdbcb4220d8c1e15
ehaigh <163937157+ehaigh@users.noreply.github.com>
2025-12-28 01:32:43 +0000

    Created Log Controller Class

---
+++ LogControllerClass.py
@@ -0,0 +1,11 @@
+#source create_log_db
+
+class LogController:
+    def __init__(self):
+        pass
+    def create_log(self, logId, action, date, time, updateDatabase, logObjects):
+        log = Log(logId, action, date, time)
+        logObjects.append(log)
+        if updateDatabase:
+            create_log_db(logId, action, date, time)
+        return logObjects
