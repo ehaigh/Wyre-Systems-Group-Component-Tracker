7ff0f04ff37b4cc867d3bcca6a43bda5da776d0e
ehaigh <163937157+ehaigh@users.noreply.github.com>
2025-12-28 00:40:58 +0000

    Update EmployeeClass.py

--- EmployeeClass.py
+++ EmployeeClass.py
@@ -1,6 +1,24 @@
+#need to source hashPassword
+
 class Employee:
     def __init__(self, employeeId, username, password, isManager):
         self.employeeId = employeeId
         self.username = username
         self.password = hashPassword(password)
+        self.isManager = isManager
+    def get_employeeId(self):
+        return self.employeeId
+    def get_username(self):
+        return self.username
+    def get_password(self):
+        return self.password
+    def get_isManager(self):
+        return self.isManager
+    def set_employeeId(self, employeeId):
+        self.employeeId = employeeId
+    def set_username(self, username):
+        self.username = username
+    def set_password(self, password):
+        self.password = hashPassword(password)
+    def set_isManager(self, isManager):
         self.isManager = isManager
