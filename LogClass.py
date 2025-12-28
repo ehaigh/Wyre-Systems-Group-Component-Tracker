1e0f47f345364c6182223b78116569476c3215a3
ehaigh <163937157+ehaigh@users.noreply.github.com>
2025-12-28 00:42:40 +0000

    Update LogClass.py

--- LogClass.py
+++ LogClass.py
@@ -4,3 +4,19 @@ class Log:
         self.action = action
         self.date = date
         self.time = time
+    def get_logId(self):
+        return self.logId
+    def get_action(self):
+        return self.action
+    def get_date(self):
+        return self.date
+    def get_time(self):
+        return self.time
+    def set_logId(self, logId):
+        self.logId = logId
+    def set_action(self, action):
+        self.action = action
+    def set_date(self, date):
+        self.date = date
+    def set_time(self, time):
+        self.time = time
