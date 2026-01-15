a747e8cd8a8cae9440977a74f0266882e721dd9b
ehaigh <163937157+ehaigh@users.noreply.github.com>
2026-01-15 16:37:10 +0000

    Update DatabaseReaderClass.py

--- DatabaseReaderClass.py
+++ DatabaseReaderClass.py
@@ -10,32 +10,25 @@ class DatabaseReader:
         self.connection.close()
 
     def load_all(self):
-        employeeObjects = self.load_employees()
-        componentObjects = self.load_components()
-        logObjects = self.load_logs()
-        return employeeObjects, componentObjects, logObjects
+        self.load_employees()
+        self.load_components()
+        self.load_logs()
     def load_employees(self):
-        employeeObjects = []
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM UserTable")
         for row in cursor.fetchall():
             isActive = bool(row[3])
-            employeeObjects = create_employee(row[0], row[1], row[2], isActive, False, employeeObjects)
-        return employeeObjects
+            create_employee(row[0], row[1], row[2], isActive, False)
     def load_components(self):
-        componentObjects = []
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM ComponentTable")
         for row in cursor.fetchall():
-            componentObjects = create_component(row[0], row[1], row[2], row[3], row[4], False, componentObjects)
-        return componentObjects
+            create_component(row[0], row[1], row[2], row[3], row[4], False)
     def load_logs(self):
-        logObjects = []
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM LogTable")
         for row in cursor.fetchall():
-            logObjects = create_log(row[0], row[1], row[2], row[3], False, logObjects)
-        return logObjects
+            create_log(row[0], row[1], row[2], row[3], False)
         
 
 
