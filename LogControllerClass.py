1fcd68f1a1a798eb0a11055a921812f7d21e0b11
ehaigh <163937157+ehaigh@users.noreply.github.com>
2026-01-16 15:19:12 +0000

    Update LogControllerClass.py

--- LogControllerClass.py
+++ LogControllerClass.py
@@ -1,13 +1,18 @@
-from DatabaseWriterClass import create_log_db
+from LogClass import Log
+from DatabaseWriterClass import DatabaseWriter
 
 class LogController:
     def __init__(self):
         self.logObjects = []
-    def create_log(self, logId, action, date, time, updateDatabase):
+        self.db = DatabaseWriter()
+
+    def create_log(self, logId, action, date, time, updateDatabase=True):
         log = Log(logId, action, date, time)
         self.logObjects.append(log)
+
         if updateDatabase:
-            create_log_db(logId, action, date, time)
+            self.db.create_log_db(logId, action, date, time)
 
     def get_log_objects(self):
         return self.logObjects
+
