37eb98d7a60dcf9a771281a1eb0514c69294b09e
ehaigh <163937157+ehaigh@users.noreply.github.com>
2026-01-16 15:20:41 +0000

    Update DatabaseReaderClass.py

--- DatabaseReaderClass.py
+++ DatabaseReaderClass.py
@@ -1,35 +1,42 @@
-from EmployeeControllerClass import create_employee
-from ComponentControllerClass import create_component
-from LogControllerClass import create_log
+import sqlite3
 
 class DatabaseReader:
-    def __init__(self):
-        self.connection = sqlite3.connect('database.db')
-
-    def close(self):
-        self.connection.close()
+    def __init__(self, employeeController, componentController, logController):
+        self.connection = None
+        self.employeeController = employeeController
+        self.componentController = componentController
+        self.logController = logController
 
     def load_all(self):
+        self.connection = sqlite3.connect('database.db')
         self.load_employees()
         self.load_components()
         self.load_logs()
+        self.connection.close()
+        self.connection = None
+
     def load_employees(self):
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM UserTable")
         for row in cursor.fetchall():
-            isActive = bool(row[3])
-            create_employee(row[0], row[1], row[2], isActive, False)
+            employeeId, username, password, isManager = row
+            self.employeeController.create_employee(employeeId, username, password, isManager, updateDatabase=False)
+
     def load_components(self):
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM ComponentTable")
         for row in cursor.fetchall():
-            create_component(row[0], row[1], row[2], row[3], row[4], False)
+            componentId, componentName, quantity, minQuantity, status = row
+            self.componentController.create_component(componentId, componentName, quantity, minQuantity, status, updateDatabase=False)
+
     def load_logs(self):
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM LogTable")
         for row in cursor.fetchall():
-            create_log(row[0], row[1], row[2], row[3], False)
+            logId, action, date, time = row
+            self.logController.create_log(logId, action, date, time, updateDatabase=False)
         
+
 
 
 
