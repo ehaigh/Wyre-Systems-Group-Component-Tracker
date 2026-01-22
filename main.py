c899f4b66804f196019cd6b6ea6f4549b4e7b82c
ehaigh <163937157+ehaigh@users.noreply.github.com>
2026-01-22 21:48:02 +0000

    Added comments

--- main.py
+++ main.py
@@ -7,28 +7,34 @@ from DatabaseReaderClass import DatabaseReader
 from UIControllerClass import UIController
 from BackupControllerClass import BackupController
 
+#Create controller objects
 employeeController = EmployeeController()
 componentController = ComponentController()
 logController = LogController()
 backupController = BackupController()
 
+#Create utility controller
 utils = UtilitiesController(
     employeeController=employeeController,
     componentController=componentController,
     logController=logController,
     backupController=backupController
 )
-
+#Create database reader
 dbReader = DatabaseReader(
     employeeController=employeeController,
     componentController=componentController,
     logController=logController
 )
+#Initialise the system
 utils.initialiseSystem(dbReader)
 
+#Create the UI window
 root = tk.Tk()
+#Initialise the UI
 ui = UIController(root, utils)
 utils.setUIController(ui)
 ui.initialiseUI() 
 
+#necessary for UI to execute
 root.mainloop()
